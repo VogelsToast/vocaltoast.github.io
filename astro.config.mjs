ebc36c67c3c1640494730a24da8f368bbb17e2c8
Zac Culpan <zacculpan@gmail.com>
2026-03-20 21:47:48 +1300

    Fix base path trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://vogelstoast.github.io',
-  base: '/vocaltoast.github.io',
+  base: '/vocaltoast.github.io/',
 });
